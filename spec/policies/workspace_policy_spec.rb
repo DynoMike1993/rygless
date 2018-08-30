3987a065f33cbeea9eac55287ddf9b99961066f0
Dawid Rygielski <dawid.rygielski@netguru.pl>
2018-08-30 17:33:12 +0200

    work

--- spec/policies/workspace_policy_spec.rb
+++ spec/policies/workspace_policy_spec.rb
@@ -6,6 +6,7 @@ describe WorkspacePolicy do
   let(:workspace) { build(:workspace) }
   let(:user) { build(:user) }
 
+  
   permissions :edit? do
     it 'denies access if workspace doesnt belongs to user' do
       expect(subject).not_to permit(user, workspace)
